560b0318ea38ab3f726481d3ee1e1bbe66fa8544
stoked-security <erwinp@dds.nl>
2017-01-29 14:28:43 -0800

    Fix libortp package name.

--- build.sh
+++ build.sh
@@ -109,7 +109,7 @@ installIfMissing libsqlite3-dev
 installIfMissing libusb-1.0-0
 installIfMissing libusb-1.0-0-dev
 installIfMissing libortp-dev
-installIfMissing libortp9
+installIfMissing libortp8
 installIfMissing libosip2-dev
 installIfMissing libreadline-dev
 installIfMissing libncurses5
